9486a8c38bb8ab2419b77306e36088770f50c942
Ciuis <denis.v.krasnikov@gmail.com>
2022-01-30 14:48:59 +0300

    done with switch getstr.h should be replaced with get_string from cs50

--- scrable.c
+++ scrable.c
@@ -1,7 +1,6 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 #include "getstr.h"
 
 // Points assigned to each letter of the alphabet
@@ -12,27 +11,60 @@ int compute_score(char* word);
 int main(void) {
     // Get input words from both players
     char* word1;
+    char* word2;
     int len;
+    printf("Player 1: ");
     word1 = get_string(&len);
-
-    for (int i = 0; i < strlen(word1); ++i) {
-        if ((char)isupper(word1[i])) {
-            word1[i] = (char)tolower(word1[i]);
-        }
-    }
-
-    printf("%s ", word1);
-
-    //char* word2;
-    //scanf("Player 2: ", word2);
-
+    printf("Player 2: ");
+    word2 = get_string(&len);
 
 
     // Score both words
-    //int score1 = compute_score(word1);
-    //int score2 = compute_score(word2);
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
 
-    // TODO: Print the winner
+    printf("%s", score1 > score2 ? "Player 1 wins\n" : "Player 2 wins");
 
     return 0;
+}
+
+int compute_score(char* word) {
+    for (int i = 0; i < strlen(word); ++i) {
+        if ((char)isupper(word[i])) {
+            word[i] = (char)tolower(word[i]);
+        }
+    }
+
+    int score = 0;
+    for (int i = 0; i < strlen(word); ++i) {
+        int n = 0;
+
+        // I'm sure it's not meet expectations but why not
+        switch(word[i]) {
+            case 'a': case 'e': case 'i': case 'l': case 'n': case 'o': case 'r': case 's': case 't': case 'u':
+                n = 1;
+                break;
+            case 'd': case 'g':
+                n = 2;
+                break;
+            case 'b': case 'c': case 'm': case 'p':
+                n = 3;
+                break;
+            case 'f': case 'h': case 'v': case 'w': case 'y':
+                n = 4;
+                break;
+            case 'k':
+                n = 5;
+                break;
+            case 'j': case 'x':
+                n = 8;
+                break;
+            case 'q': case 'z':
+                n = 10;
+                break;
+        }
+        score += n;
+    }
+
+    return score;
 }
